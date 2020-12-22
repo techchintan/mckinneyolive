65d935de5c49d366c4b62fb0515b2b632de00e7b
Carlos Gonzalez <homearanya@gmail.com>
2020-12-22 20:55:14 +0200

    Change Favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,10 +40,10 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#0A5587`,
-        theme_color: `#0A5587`,
+        background_color: `#005586`,
+        theme_color: `#005586`,
         display: `minimal-ui`,
-        icon: `src/images/favicon.svg`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     {
