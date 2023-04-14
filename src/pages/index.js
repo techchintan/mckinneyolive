69d2d26a9f954fbaa91673daa4d6dea88c3838b0
June Leoman Lapera <leomanlapera@gmail.com>
2023-04-14 10:18:24 +0800

    attachment issue

--- src/pages/index.js
+++ src/pages/index.js
@@ -198,6 +198,15 @@ export const query = graphql`
           title
           content {
             raw
+            references {
+              ... on ContentfulAsset {
+                contentful_id
+                __typename
+                file {
+                  url
+                }
+              }
+            }
           }
           time
           location
